6ff4f825187c87a404a7337a638140f6a65d68a3
Terry Schneider <tnschneider@gmail.com>
2017-06-30 17:52:59 -0500

    Completed project 04

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+	@R2
+	M=0
+
+(LOOP)
+	@R1
+	D=M
+	@END
+	D;JEQ
+
+	@R1
+	M=M-1
+
+	@R0
+	D=M
+
+	@R2
+	M=M+D
+
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
